81dc9b3403c982a0d1aa797a096ff13eb9a14b45
Yuji <yuji08752@gmail.com>
2023-12-03 22:33:20 +0900

    linterの実行

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // 必要な関数を import
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyChttzaxXeiZaEwe-Wp2COLZ3caBqFwDro",
@@ -9,9 +9,9 @@ const firebaseConfig = {
   storageBucket: "dlbia-a5d79.appspot.com",
   messagingSenderId: "539836875443",
   appId: "1:539836875443:web:9e39400420a322f06d66c6",
-};
+}
 
 // Firebaseアプリオブジェクトを初期化
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 // Firestoreを読み込み、db(databaseの略)として export
-export const db = getFirestore(app);
+export const db = getFirestore(app)
