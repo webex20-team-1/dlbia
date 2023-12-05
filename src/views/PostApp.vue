7cbc7d9858c30c1fe7cd845b43ae703b3fa149e8
Ryo-nishioka <nishioka.r1001@gmail.com>
2023-12-05 16:00:59 +0900

    投稿ボタンを作成した

--- src/views/PostApp.vue
+++ src/views/PostApp.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="post-page">
     <h1>投稿</h1>
+    <div class="home__wrapper">
+      <div class="form__wrapper">
+        <textarea
+          class="form__textarea"
+          v-model="text"
+          placeholder="いまどうしてる？"
+        />
+        <div class="form__buttons">
+          <button v-on:click="postTweet" class="form__submit-button">
+            投稿
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    }
+  },
+  methods: {
+    postTweet() {
+      alert("投稿機能の完成をお楽しみに！")
+    },
+  },
+}
+</script>
+
+<style scoped>
+.home__wrapper {
+  margin: 0 auto;
+  max-width: 600px;
+  background-color: #ccc;
+}
+.form__wrapper {
+  padding: 1rem;
+}
+.form__textarea {
+  width: 100%;
+  height: calc(1.3rem * 3 + 0.5rem * 2);
+  padding: 0.5rem;
+  line-height: 1.3rem;
+  border-radius: 5px;
+  border: none;
+  resize: none;
+}
+.form__textarea:focus {
+  outline: none;
+}
+.form__buttons {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
