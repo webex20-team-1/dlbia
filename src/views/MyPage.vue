32712556eb64fa571771f3d2e3f2d159e535e7d1
aintnofastlife <yusukemiyamura2025@gmail.com>
2023-12-11 08:03:18 +0900

    ログインできるようにしました

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -2,4 +2,22 @@
   <div>
     <h1>My Page</h1>
   </div>
+  <div>
+    <button v-on:click="signIn">サインインする</button>
+  </div>
 </template>
+
+<script>
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth"
+
+export default {
+  methods: {
+    signIn: function () {
+      // 以下の Firebase に関するコードは、公式ドキュメントをコピペしただけ
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      signInWithRedirect(auth, provider)
+    },
+  },
+}
+</script>
