6cba4784796ad78fe3651f682c65c343d61ee30b
Ryo-nishioka <nishioka.r1001@gmail.com>
2023-12-13 11:17:16 +0900

    作業

--- src/views/PostApp.vue
+++ src/views/PostApp.vue
@@ -11,9 +11,13 @@
         />
         <textarea class="form__textarea" v-model="url" placeholder="url" />
         <div class="form__buttons">
-          <button v-on:click="postTweet" class="form__submit-button">
+          <button
+            v-on:click="postTweet, showModal()"
+            class="form__submit-button"
+          >
             投稿
           </button>
+          <modal :message="message" v-show="modal"></modal>
         </div>
       </div>
     </div>
@@ -24,7 +28,12 @@
 import { collection, addDoc } from "firebase/firestore"
 // firebase.js で db として export したものを import
 import { db } from "@/firebase.js"
+import Modal from "./Modal.vue"
 export default {
+  name: "HelloWorld",
+  components: {
+    Modal,
+  },
   data() {
     return {
       posts: [
@@ -33,9 +42,19 @@ export default {
         //   text: "こんにちは、ツイートの本文です。"
         // }
       ],
+
+      message: "よろしいですか？",
+      modal: false,
     }
   },
+  props: {
+    msg: String,
+  },
   methods: {
+    showModal() {
+      // モーダル表示する際の処理が必要ならここに書く
+      this.modal = true
+    },
     postTweet() {
       /* 変更点１ */
       addDoc(collection(db, "posts"), {
